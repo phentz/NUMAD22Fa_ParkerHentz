d07fe69b5bc1649e88dbf8f3fc140d075bd6df2f
Parker H <parkerhentz@gmail.com>
2022-10-03 16:06:07 -0400

    HW4 adding in link collector page connected to main page

--- app/src/main/java/edu/northeastern/numad22fa_parkerhentz/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_parkerhentz/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button click_button;
     Button aboutme;
+    Button linkCollector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +21,36 @@ public class MainActivity extends AppCompatActivity {
 
         click_button = findViewById(R.id.Button_suite);
         click_button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
                 finish();
+            }
+        });
 
+
+        aboutme = findViewById(R.id.aboutme);
+        aboutme.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
                 Intent intent2 = new Intent(MainActivity.this, aboutmepage.class);
+                startActivity(intent2);
+                finish();
+
+
+            }
+        });
+
+
+        linkCollector = findViewById(R.id.linkCollector);
+        linkCollector.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, linkCollector.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/edu/northeastern/numad22fa_parkerhentz/aboutmepage.java
+++ app/src/main/java/edu/northeastern/numad22fa_parkerhentz/aboutmepage.java
@@ -10,5 +10,6 @@ public class aboutmepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutmepage);
+
     }
 }
