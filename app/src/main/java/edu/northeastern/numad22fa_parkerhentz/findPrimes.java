1da2956030fd05ffbfdbeac352bd922a4be90787
Parker H <parkerhentz@gmail.com>
2022-10-09 14:50:20 -0600

    all of the app is working- I am exausted

--- app/src/main/java/edu/northeastern/numad22fa_parkerhentz/findPrimes.java
+++ app/src/main/java/edu/northeastern/numad22fa_parkerhentz/findPrimes.java
@@ -17,7 +17,7 @@ public class findPrimes extends AppCompatActivity {
     Button button_ender;
     int valueCheck = 3;
     int LastfoundPrime = valueCheck;
-
+    public boolean terminated = false;
 
     @Override
     public void onBackPressed() {
@@ -25,8 +25,7 @@ public class findPrimes extends AppCompatActivity {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Closing Activity")
                 .setMessage("Are you sure you want to close this activity?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
-                {
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         finish();
@@ -42,11 +41,11 @@ public class findPrimes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_primes);
         statusText = findViewById(R.id.statusText);
-        button_ender =findViewById(R.id.button2endSearch);
+        button_ender = findViewById(R.id.button2endSearch);
         button_starter = findViewById(R.id.button_start);
 
         button_starter.setOnClickListener(new View.OnClickListener() {
-           // first thread start from the thread class
+            // first thread start from the thread class
             @Override
             public void onClick(View view) {
                 DifferentThread differentThread = new DifferentThread();
@@ -59,11 +58,9 @@ public class findPrimes extends AppCompatActivity {
         button_ender.setOnClickListener(new View.OnClickListener() {
             // Cant be here cant be placed inside the clas
             // DifferentThread differentThread = new DifferentThread();
-            // differentThread.start();
             @Override
             public void onClick(View view) {
-                DifferentThread differentThread = new DifferentThread();
-                differentThread.interrupt();
+                terminated = true;
                 // value2 = here we need
 
                 // if(valueCheck
@@ -71,8 +68,6 @@ public class findPrimes extends AppCompatActivity {
             }
         });
     }
-
-
     //Method which runs on a different thread which uses Thread class.
     public void runOnDifferentThread(View view) {
         DifferentThread differentThread = new DifferentThread();
@@ -83,30 +78,28 @@ public class findPrimes extends AppCompatActivity {
     class DifferentThread extends Thread {
         @Override
         public void run() {
-            for (int i = valueCheck; i <= 1000; i = i + 2) {
-                if (checkPrime(i)) {
-                    LastfoundPrime = i;
+
+            for (int i = valueCheck; i <= 1000 && !terminated; i = i + 2 ) {
+                    if (checkPrime(i)) {
+                        LastfoundPrime = i;
+                    }
+                    int finalI = i;
+                    // variable needs to be thread safe
+                    // look up^ copy to
+                    // syconisity different value
+                    runOnUiThread(() -> {
+                        //System.out.print(valueCheck + "here");
+                        // never gets changed
+                        statusText.setText("started process checking: " + finalI +
+                                "  Last Prime :" + LastfoundPrime);
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                int finalI = i;
-
-                // variable needs to be thread safe
-                // look up^ copy to
-                // syconisity different value
-                runOnUiThread(() -> {
-                    //System.out.print(valueCheck + "here");
-                    // never gets changed
-                    statusText.setText("started process checking: " + finalI +
-                            "  Last Prime :" + LastfoundPrime );
-
-
-                });
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
+           // }
         }
     }
 
