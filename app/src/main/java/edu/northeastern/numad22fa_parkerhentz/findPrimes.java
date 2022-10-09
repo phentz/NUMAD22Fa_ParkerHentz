da1dfa9e638561dc2a8b9dc2418ccc0b6d492cfb
Parker H <parkerhentz@gmail.com>
2022-10-09 11:31:56 -0600

    Got the threads working buttons start and interupt the processes....not showing the loop running thorugh the values though... need help with that and rotation.

--- app/src/main/java/edu/northeastern/numad22fa_parkerhentz/findPrimes.java
+++ app/src/main/java/edu/northeastern/numad22fa_parkerhentz/findPrimes.java
@@ -1,45 +1,73 @@
 package edu.northeastern.numad22fa_parkerhentz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.annotation.SuppressLint;
-import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class findPrimes extends AppCompatActivity {
-    private Handler textHandler = new Handler(); // just a reg handler
-    TextView statusText;
+    private final Handler textHandler = new Handler(); // just a reg handler
+    TextView statusText; // here is where it wil update the text
     Button button_starter;
+    Button button_ender;
     int valueCheck = 3;
     int valuePast = valueCheck;
 
 
     @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Closing Activity")
+                .setMessage("Are you sure you want to close this activity?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_primes);
-        statusText = findViewById(R.id.textView);
-
+        statusText = findViewById(R.id.statusText);
+        button_ender =findViewById(R.id.button2endSearch);
         button_starter = findViewById(R.id.button_start);
-
 
         button_starter.setOnClickListener(new View.OnClickListener() {
             // Cant be here cant be placed inside the clas
             // DifferentThread differentThread = new DifferentThread();
             // differentThread.start();
-
             @Override
             public void onClick(View view) {
-                statusText.setText("Current prime check" + statusText);
                 DifferentThread differentThread = new DifferentThread();
                 differentThread.start();
+                // value2 = here we need
+                statusText.setText("Current prime check: "+ valueCheck);
+            }
+        });
+
+        button_ender.setOnClickListener(new View.OnClickListener() {
+            // Cant be here cant be placed inside the clas
+            // DifferentThread differentThread = new DifferentThread();
+            // differentThread.start();
+            @Override
+            public void onClick(View view) {
+                DifferentThread differentThread = new DifferentThread();
+                differentThread.interrupt();
+                // value2 = here we need
+                statusText.setText("ended process: "+ valueCheck);
             }
         });
     }
@@ -58,9 +86,11 @@ public class findPrimes extends AppCompatActivity {
         public void run() {
             for (int i = valueCheck; i <= 1000; i = i + 2) {
                 checkPrime(i);
-                statusText.setText("checking Current Prime " + i);
+                System.out.print(i + "here");
+
+                //statusText.setText("checking Current Prime " + i);
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -84,23 +114,22 @@ public class findPrimes extends AppCompatActivity {
         }
     }
 
-
-    static void checkPrime(int n) {
+    static boolean checkPrime(int n) {
         int i, m = 0, flag = 0;
         m = n / 2;
         if (n == 0 || n == 1) {
-            System.out.println(n + " is not prime number");
+            //System.out.println(n + " is not prime number");
+            return false;
         } else {
             for (i = 2; i <= m; i++) {
                 if (n % i == 0) {
-                    System.out.println(n + " is not prime number");
                     flag = 1;
-                    break;
+                    return false;
+                    //System.out.println(n + " is not prime number");
                 }
             }
-            if (flag == 0) {
-                System.out.println(n + " is prime number");
-            }
+            //System.out.println(n + " is prime number");
+            return flag == 0;
 
         }
     }
